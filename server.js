d85b23d55ca115ff1d04cac8e335c3c595022f92
Ryan Brodsky <ryan.brodsky@generalassemb.ly>
2024-03-06 16:59:19 -0700

    edits from qa1

---
+++ server.js
@@ -0,0 +1,25 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const testJWTRouter = require('./controllers/test-jwt');
+const usersRouter = require('./controllers/users');
+const profilesRouter = require('./controllers/profiles');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(express.json());
+
+// Routes go here
+app.use('/test-jwt', testJWTRouter);
+app.use('/users', usersRouter);
+app.use('/profiles', profilesRouter);
+
+app.listen(3000, () => {
+    console.log('The express app is ready!');
+});
